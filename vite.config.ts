36620f27226a745a36d9cfae39fd2ea9884fa1ff
Ricardo Urbina <ricardojurbina.c@gmail.com>
2026-04-02 15:02:21 -0500

    Mejoras de carga

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,28 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        // Usamos una función para definir los chunks, lo que resuelve el error de tipos
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) {
+              return 'vendor-react';
+            }
+            if (id.includes('framer-motion')) {
+              return 'vendor-motion';
+            }
+            return 'vendor-others';
+          }
+        },
+      },
+    },
+    // Optimizamos el límite para evitar avisos innecesarios en builds complejos
+    chunkSizeWarningLimit: 800,
+  },
 })
