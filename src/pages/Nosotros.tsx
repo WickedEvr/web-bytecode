2e2bae718ddab70a4a4b61415c2d4185eb097391
Luis Laurente <luis149812@gmail.com>
2026-04-18 00:37:30 -0500

    Vista movil home 1

--- src/pages/Nosotros.tsx
+++ src/pages/Nosotros.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import AltFooter from '../components/AltFooter';
+import SEO from '../components/SEO';
 
 const infoBlocks = [
   {
